5788957137ddf4dec40a479d1124b1952b7e4213
Dhruv Kumar Singh <dkmain12@gmail.com>
2024-12-19 23:34:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
-// // App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Navbar from './components/Navbar';
-// import Home from './pages/Home';
-// import AboutCancer from './pages/AboutCancer';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import DetectCancer from './pages/DetectCancer';
-// import Footer from './components/Footer';
-// import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AboutCancer from "./pages/AboutCancer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import DetectCancer from "./pages/DetectCancer";
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <div>
-//         <Navbar />
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/about-cancer" element={<AboutCancer />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/register" element={<Register />} />
-//           <Route path="/detect" element={<DetectCancer />} />
-//         </Routes>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-// App.js
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import AboutCancer from './pages/AboutCancer';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import DetectCancer from './pages/DetectCancer';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutCancer />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/detect" element={<DetectCancer />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={AboutCancer} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/detect" component={DetectCancer} />
+      </Switch>
     </Router>
   );
-}
+};
 
 export default App;
